f10d87393f18f61b724787057a21d9e1b4125d4c
manojK <manojksangwan@gmail.com>
2016-11-04 16:08:26 +0530

    from office

--- app/src/main/java/com/example/manojk/myfirstapp/IResult.java
+++ app/src/main/java/com/example/manojk/myfirstapp/IResult.java
@@ -9,7 +9,10 @@ import org.json.JSONObject;
  * Created by manojK on 18/08/2016.
  */
 public interface IResult {
+
     public void notifySuccess(String requestType,JSONObject response);
+
     public void notifySuccess(String requestType,JSONArray response);
+
     public void notifyError(String requestType,VolleyError error);
 }
