92574fc0b70f3a91d5da89cfb50fd650aaa17cce
manojK <manojksangwan@gmail.com>
2016-11-27 14:10:15 +0530

    home

--- app/src/main/java/com/example/manojk/ors/CustomerDetails.java
+++ app/src/main/java/com/example/manojk/ors/CustomerDetails.java
@@ -27,6 +27,7 @@ import com.example.manojk.ors.Models.MerchantHandler;
 import com.example.manojk.ors.Models.orsAvailableServices;
 import com.hdfcmerchant.PayActivity;
 
+
 public class CustomerDetails extends AppCompatActivity {
     orsAvailableServices orsAS;
     TextView tv_tripRoute,tv_jTime1;
@@ -167,7 +168,7 @@ public class CustomerDetails extends AppCompatActivity {
 
         new MakeMessageHashTask().execute();
     }
-
+/*
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == WibmoSDK.REQUEST_CODE_IAP_PAY) {
@@ -221,7 +222,7 @@ public class CustomerDetails extends AppCompatActivity {
             outputView.setText(sb.toString());
         }// requestCode
     }//onActivityResult
-
+*/
 
 
     private class MakeMessageHashTask extends AsyncTask<Void, Void, Void> {
@@ -286,17 +287,58 @@ public class CustomerDetails extends AppCompatActivity {
         bundle.putString("payerIFSC", "");
         bundle.putString("payerAadhaarNo", "");
         bundle.putString("payerMobileNo", "");
-        bundle.putString("payerPaymentAddress", "");
+        bundle.putString("payerPaymentAddress", "Director General State Transport Haryana");
         bundle.putString("subMerchantID", "");
         bundle.putString("payerMMID", "");
         bundle.putString("payeeMMID", "");
         bundle.putString("refurl", "");
         bundle.putString("amount","1122.00");
+        bundle.putString("add1", "");
+        bundle.putString("add2", "");
+        bundle.putString("add3", "");
+        bundle.putString("add4", "");
+        bundle.putString("add5", "");
+        bundle.putString("add6", "");
+        bundle.putString("add7", "");
+        bundle.putString("add8", "");
+        bundle.putString("add9", "");
+        bundle.putString("add10", "");
 
         Intent intent = new Intent(getApplicationContext(), PayActivity.class);
         intent.putExtras(bundle);
         startActivityForResult(intent, 1);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK && data != null)
+        {
+            Bundle bundle = getIntent().getExtras();
+            String pgMeTrnRefNo = bundle.getString("pgMeTrnRefNo");
+            String orderNo = bundle.getString("orderNo");
+            String txnAmount = bundle.getString("txnAmount");
+            String tranAuthdate = bundle.getString("tranAuthdate");
+            String statusCode = bundle.getString("status");
+            String statusDesc = bundle.getString("statusDesc");
+            String responsecode = bundle.getString("responsecode");
+            String approvalCode = bundle.getString("approvalCode");
+            String payerVA = bundle.getString("payerVA");
+            String npciTxnId = bundle.getString("npciTxnId");
+            String refId = bundle.getString("refId");
+            String add1 = bundle.getString("add1");
+            String add2 = bundle.getString("add2");
+            String add3 = bundle.getString("add3");
+            String add4 = bundle.getString("add4");
+            String add5 = bundle.getString("add5");
+            String add6 = bundle.getString("add6");
+            String add7 = bundle.getString("add7");
+            String add8 = bundle.getString("add8");
+            String add9 = bundle.getString("add9");
+            String add10 = bundle.getString("add10");
+
+            outputView.setText(statusCode + "  " + responsecode);
+        }
+    }
 }
-
-
